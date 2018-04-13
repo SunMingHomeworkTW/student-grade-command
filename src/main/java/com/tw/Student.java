48e7c55d3ffc2439d8d8d1dbacbdf243c67a5328
SunMing <1229536801@qq.com>
2018-04-13 14:04:30 +0800

    完成了Student类

---
+++ src/main/java/com/tw/Student.java
@@ -0,0 +1,83 @@
+package com.tw;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Student {
+  private String id;
+  private String name;
+  private Map<String,Integer> scores;
+  private int totalScore;
+  private double averageScore;
+
+  public Student(){}
+
+  public Student(String studentInfo) {
+    String[] strs=studentInfo.split("，");
+    name=strs[0];
+    id=strs[1];
+    scores=new LinkedHashMap<>();
+    for (int i = 0; i < strs.length - 2; i++) {
+      String[] strs1=strs[i+2].split("：");
+      scores.put(strs1[0],Integer.valueOf(strs1[1]));
+    }
+    totalScore=getTotalScoreOfAStudent(scores);
+    averageScore=getAverageScoreOfAStudent(scores);
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Map<String, Integer> getScores() {
+    return scores;
+  }
+
+  public void setScores(Map<String, Integer> scores) {
+    this.scores = scores;
+  }
+
+  public double getAverageScore() {
+    return averageScore;
+  }
+
+  public void setAverageScore(double averageScore) {
+    this.averageScore = averageScore;
+  }
+
+  public double getTotalScore() {
+    return totalScore;
+  }
+
+  public void setTotalScore(int totalScore) {
+    this.totalScore = totalScore;
+  }
+
+
+  public int getTotalScoreOfAStudent(Map<String, Integer> scores) {
+    int sum=0;
+    for(int score:scores.values())
+      sum+=score;
+    return sum;
+  }
+
+  public double getAverageScoreOfAStudent(Map<String, Integer> scores) {
+    return (double) getTotalScoreOfAStudent(scores)/scores.size();
+  }
+
+  public String getDetailedInfo(){
+    return name+"|"+scores.get("数学")+"|"+scores.get("语文")+"|"+scores.get("英语")+"|"+scores.get("编程")+"|"+averageScore+"|"+totalScore;
+  }
+}
--- src/test/java/com/tw/StudentTest.java
+++ src/test/java/com/tw/StudentTest.java
@@ -1,34 +1,43 @@
-//package com.tw;
-//
-//import static org.junit.Assert.*;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//import static org.fest.assertions.api.Assertions.assertThat;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//import org.junit.Test;
-//
-//public class StudentTest {
-//  private  Student student;
-//
-//  @Test
-//  public void testGetAverageScoreOfAStudent() {
-//    Map<String,Integer> scores=new HashMap<>();
-//    scores.put("数学",75);
-//    scores.put("数学",75);
-//    scores.put("数学",75);
-//    scores.put("数学",75);
-//    assertThat(student.getAverageScoreOfAStudent(scores)).isEqualTo(82.5);
-//  }
-//
-//  @Test
-//  public void testGetTotalScoreOfAStudent() {
-//    Map<String,Integer> scores=new HashMap<>();
-//    scores.put("数学",75);
-//    scores.put("数学",75);
-//    scores.put("数学",75);
-//    scores.put("数学",75);
-//    assertThat(student.getTotalScoreOfAStudent(scores)).isEqualTo(330);
-//  }
-//}
+package com.tw;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+
+public class StudentTest {
+  private  Student student;
+
+  @Test
+  public void testGetAverageScoreOfAStudent() {
+    Map<String,Integer> scores=new HashMap<>();
+    scores.put("数学",75);
+    scores.put("语文",95);
+    scores.put("英语",80);
+    scores.put("编程",80);
+    student=new Student();
+    assertThat(student.getAverageScoreOfAStudent(scores)).isEqualTo(82.5);
+  }
+
+  @Test
+  public void testGetTotalScoreOfAStudent() {
+    Map<String,Integer> scores=new HashMap<>();
+    scores.put("数学",75);
+    scores.put("语文",95);
+    scores.put("英语",80);
+    scores.put("编程",80);
+    student=new Student();
+    assertThat(student.getTotalScoreOfAStudent(scores)).isEqualTo(330);
+  }
+
+  @Test
+  public void testGetDetailedInfo() {
+    String studentInfo="张三，0001，数学：75，语文：95，英语：80，编程：80";
+    student=new Student(studentInfo);
+    assertThat(student.getDetailedInfo()).isEqualTo("张三|75|95|80|80|82.5|330");
+  }
+}
